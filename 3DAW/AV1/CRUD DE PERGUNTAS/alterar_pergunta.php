a656d6b847e9365337b5943e1aa4156f0ba14de3
Amilcar Ribeiro Rodrigues Júnior <166032111+AmilcarRibeiroRodriguesJunior@users.noreply.github.com>
2025-09-22 16:18:25 -0300

    Update alterar_pergunta.php

--- 3DAW/AV1/CRUD DE PERGUNTAS/alterar_pergunta.php
+++ 3DAW/AV1/CRUD DE PERGUNTAS/alterar_pergunta.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Alterar Pergunta de Múltipla Escolha</title>
+</head>
+<body>
+</html>
+
 <?php
 if(!isset($_GET['id'])){ 
     echo "ID não especificado."; 
